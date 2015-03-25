055c57d066f9b76833273f9fc3e604055b8e3aeb
Martin Ivanov Matev <martin.matev89@gmail.com>
2015-03-26 00:40:15 +0200

    Style tab update
    Пооправих Style tab-а, избягвайки app.config :)
    Добавих User friendly бутон, освен КомбоБокса, който стои неактивен,
    докато не се избере стил.
    Все още не е довършено напълно, но утре ще го tweak-на през деня или
    след работа.
    
    - TO DO list
    * Да разкарам ненужните данни от Style методите в Document Generator и
    да ги разпределя по съответните класове
    * Да махна зависимостта на Istyle интерфейса от StylePattern класа (what
    was I thinking...)
    * Да пия по-малко, защото вече не си оцелвам копчетата на клавиатурата.

--- MelonLibrary/ClassLibrary/DefaultPattern.cs
+++ MelonLibrary/ClassLibrary/DefaultPattern.cs
@@ -1,11 +1,12 @@
 ﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
 
 namespace MelonLibrary
 {
     public class DefaultPattern : StylePattern, IStylePattern
     {
-        public DefaultPattern(Font font, PageSize pageSize, Image header, bool pageNumber)
-            : base(font, pageSize, header, pageNumber)
+        public DefaultPattern(Font font, Document pageSize, Image header, bool pageNumber, StylePattern.PatternType patternType)
+            : base(font, pageSize, header, pageNumber, patternType)
         {
         }
     }
