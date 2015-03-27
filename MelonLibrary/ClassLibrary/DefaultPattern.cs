7aa47c2eca2091edb3ccd5c2711808f43fa3c79a
Martin Matev <martin.matev89@gmail.com>
2015-03-27 21:49:14 +0200

    Style update
    Разкарах ненужните данни от Style методите в Document Generator и да ги
    разпределих по съответните класове.
    Махнах зависимостта на Istyle интерфейса от StylePattern класа .

--- MelonLibrary/ClassLibrary/DefaultPattern.cs
+++ MelonLibrary/ClassLibrary/DefaultPattern.cs
@@ -1,12 +1,19 @@
-﻿using iTextSharp.text;
+﻿using System;
+using iTextSharp.text;
 using iTextSharp.text.pdf;
+using MelonLibrary.ClassLibrary.Enums;
 
 namespace MelonLibrary
 {
     public class DefaultPattern : StylePattern, IStylePattern
     {
-        public DefaultPattern(Font font, Document pageSize, Image header, bool pageNumber, StylePattern.PatternType patternType)
-            : base(font, pageSize, header, pageNumber, patternType)
+        private static readonly Document PAGE_SIZE = new Document(iTextSharp.text.PageSize.A4);
+        private static readonly Font FONT = new Font(BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1252, false), 12, Font.NORMAL);
+        private static readonly Image HEADER = Image.GetInstance(new Uri(System.IO.Path.GetFullPath("../../Images/Koala.jpg")));
+        private const bool PAGE_NUMBER = true;
+
+        public DefaultPattern()
+            : base(FONT, PAGE_SIZE, HEADER, PAGE_NUMBER, PatternType.Default)
         {
         }
     }
