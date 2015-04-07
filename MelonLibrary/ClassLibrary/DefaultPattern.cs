4bd1564dc70985d72d3c88a0a43ed2f488a11743
Martin Ivanov Matev <martin.matev89@gmail.com>
2015-04-07 21:24:58 +0300

    Nvm, downloaded the default header and added it

--- MelonLibrary/ClassLibrary/DefaultPattern.cs
+++ MelonLibrary/ClassLibrary/DefaultPattern.cs
@@ -9,7 +9,7 @@ namespace MelonLibrary
     {
         private static readonly Document PAGE_SIZE = new Document(iTextSharp.text.PageSize.A4);
         private static readonly Font FONT = new Font(BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1252, false), 12, Font.NORMAL);
-        private static readonly Image HEADER = Image.GetInstance(new Uri(System.IO.Path.GetFullPath("../../Images/Koala.jpg")));
+        private static readonly Image HEADER = Image.GetInstance(new Uri(System.IO.Path.GetFullPath("../../Images/DefaultHeader.png")));
         private const bool PAGE_NUMBER = true;
 
         public DefaultPattern()
